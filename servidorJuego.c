ed8767c980843e1de6b8a6a7c0b58662dfb45fd4
Roberto <roberto.guarneros11@gmail.com>
2022-10-11 19:34:35 +0200

    Se anade codigo del servidor para responder a registro de usuario(error al ensenar mensaje de exito)

--- servidorJuego.c
+++ servidorJuego.c
@@ -7,11 +7,77 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdbool.h>  
+#include <mysql.h>
+
+//Declaracion de funciones y metodos
+int RegisterUser(char peticion_servidor[512])
+{
+	int respuesta_servidor;//variable que se usara para control de errores
+	int codigo_peticion;
+	char username[100];
+	char password[100];
+	char name[100];
+	
+	char* p = strtok(peticion_servidor, "/");
+	int codigo = atoi(p);
+	strcpy(username,strtok(NULL, "/"));
+	strcpy(password,strtok(NULL, "/"));
+	strcpy(name,strtok(NULL, "/"));
+	
+	MYSQL *conn;
+	int err;
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	
+	char consulta [80];//string para enviar la consulta a BBDD
+	
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		respuesta_servidor = 1;//regresar 1 porque hay error al crear la conexion
+		printf ("Error al crear la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+	}
+	//inicializar la conexion
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "ProyectoSO",0, NULL, 0);
+	if (conn==NULL) {
+		respuesta_servidor=2;//error al inicializar la conexion
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+	}
+	
+	// consulta SQL para obtener una tabla con todos los datos
+	// de la base de datos
+	//construir consulta sql
+	sprintf(consulta,"INSERT INTO Jugador VALUES('%s','%s','%s');",username,password,name);
+	// hacemos la consulta 
+	err=mysql_query (conn, consulta); 
+	if (err!=0) {
+		respuesta_servidor = 3;//error al consultar datos 
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}else if(err=0)
+		respuesta_servidor=0;
+	// cerrar la conexion con el servidor MYSQL 
+	mysql_close (conn);
+	//regresamos el resultado
+	return respuesta_servidor;
+}
+
+
 
 int main(int argc, char *argv[])
 {
 	int sock_conn, sock_listen, ret;
 	struct sockaddr_in serv_adr;
+	
+	char peticion[512];
+	char peticion_servidor[512];
+	char respuesta[512];
+	int resultado;
+	
 	// INICIALIZACIONES
 	// Se abre el socket
 	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -26,16 +92,61 @@ int main(int argc, char *argv[])
 	serv_adr.sin_port = htons(9081);
 	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
 		printf ("Error al bind");
-	//Podemos atender 4 clientes al mismo tiempo.Quiere decir que estaremos escuchando al mismo tiempo para 4 clientes
+	//La cola de peticiones pendientes no podr? ser superior a 4 Que quiere decir???
 	if (listen(sock_listen, 4) < 0)
 		printf("Error en el Listen");
-	int i;
 	
-	printf("Escuchando\n");
-	int terminar = 0;
-	while(terminar==0)
+
+	int server_listening=0;
+	while(server_listening==0)
 	{
+		printf("Escuchando\n");
+		
 		sock_conn = accept(sock_listen, NULL, NULL);
 		printf("He recibido conexion\n");
+		
+		int terminar = 0;
+		while(terminar==0)
+		{
+			//Recibimos peticiones
+			ret = read(sock_conn, peticion, sizeof(peticion));
+			printf("Recibida una petición\n");
+			// Tenemos que agregar la marca de fin de string 
+			// para que no escriba lo que hay despues en el buffer
+			peticion[ret] = '\0';
+			strcpy(peticion_servidor,peticion);
+			char* p = strtok(peticion, "/");
+			int codigo = atoi(p);
+			//Escribimos la peticion en la consola
+			if (codigo != 0){
+				printf("La petición es: %s\n", peticion_servidor);
+			}
+			
+			if (codigo == 0)
+				terminar = 1;
+			else if (codigo == 1){ //registrar un usuario
+				resultado = RegisterUser(peticion_servidor);
+				printf("el resultado regreso %d",resultado);
+				if (resultado==0)
+				{
+					strcpy(respuesta, "Registrado Correctamente");
+				}
+				else
+				{
+					strcpy(respuesta, "Error en el registro");
+				}
+				
+			}
+			else if (codigo == 2)//iniciar sesion
+			{
+				
+			}
+			if (codigo !=0){
+				// Enviamos la respuesta
+				write(sock_conn,respuesta, strlen(respuesta));
+			}
+		}
+		// Se acabo el servicio para este cliente
+		close(sock_conn); 
 	}
 }
